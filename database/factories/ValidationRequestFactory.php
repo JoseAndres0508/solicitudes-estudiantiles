01b5bc3781da7e41c5fc6281dfec95dcae58f535
JoseAndres0508 <ortiz3558jo@gmail.com>
2026-08-01 12:30:07 -0600

    chore(db): scaffold domain models, migrations, factories and seeders

---
+++ database/factories/ValidationRequestFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\ValidationRequest;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<ValidationRequest>
+ */
+class ValidationRequestFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ database/seeders/WaiverRuleSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class WaiverRuleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
