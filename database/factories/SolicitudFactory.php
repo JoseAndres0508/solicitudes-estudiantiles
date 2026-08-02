ab3f7143e0ce58da8f6d2c68487ebbe81ffba772
JoseAndres0508 <ortiz3558jo@gmail.com>
2026-08-02 08:12:37 -0600

    refactor(db): align entity naming with institutional schema

---
+++ database/factories/SolicitudFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Solicitud;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Solicitud>
+ */
+class SolicitudFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ database/seeders/CatalogoConvalidacionSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CatalogoConvalidacionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
