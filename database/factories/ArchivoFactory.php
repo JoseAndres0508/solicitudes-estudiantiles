6082d2ea26ddfc6a4dbb90904c3434cbac2b74ac
JoseAndres0508 <ortiz3558jo@gmail.com>
2026-08-02 08:19:04 -0600

    chore(db): scaffold models and factories for shared tables

---
+++ database/factories/ArchivoFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Archivo;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Archivo>
+ */
+class ArchivoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ database/factories/PermisoFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Permiso;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Permiso>
+ */
+class PermisoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
